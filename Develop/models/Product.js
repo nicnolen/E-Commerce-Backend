8897e587fe49e23edcc5092a824939c4deb5b5cf
Nicholas Nolen <nicnolen@ymail.com>
2022-02-04 09:25:13 -0600

    added columns to Tag model

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -8,6 +8,7 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
+  // define columns
   {
     id: {
       type: DataTypes.INTEGER,
--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,12 +1,13 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, INTEGER } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
+  // define columns
   {
-    // define columns
+
   },
   {
     sequelize,
