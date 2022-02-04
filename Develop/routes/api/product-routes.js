5bdfd50808a538624581a5199a74599dcca12d37
Nicholas Nolen <nicnolen@ymail.com>
2022-02-04 12:10:13 -0600

    added a post route to create a new product

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -66,7 +66,13 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    tagIds: req.body.tagIds,
+  })
     .then(product => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
