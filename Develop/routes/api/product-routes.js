af30b8e07fb7e877237b9e92e0df2b87128b1bb7
Nicholas Nolen <nicnolen@ymail.com>
2022-02-09 09:30:04 -0600

    added delete route to delete a product by id

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -130,8 +130,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
+// delete one product by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then(dbProductData => {
+      if (!dbProductData) {
+        res.status(404).json({ message: 'No product found with this id!' });
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
